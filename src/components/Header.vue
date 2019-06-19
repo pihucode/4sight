d06c178831fb5cb937410424ea5ec8925ff8dc22
Pinxuan Huang <pinxhuang@gmail.com>
2019-06-19 21:02:30 +0400

    added Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: '4sight',
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+$title-color: whitesmoke;
+$title-size: 32px;
+
+header {
+    padding: 12px;
+}
+
+h1 {
+    text-align: center;
+    font-size: $title-size;
+    color: $title-color;
+}
+</style>
