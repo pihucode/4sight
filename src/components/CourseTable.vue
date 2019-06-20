310226cd6e30b2a933fde150f32fae0f7e5160af
Pinxuan Huang <pinxhuang@gmail.com>
2019-06-20 21:03:13 +0400

    started CourseTable component

---
+++ src/components/CourseTable.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+      <!-- COURSE BASKET     -->
+    <div class="course-basket">
+        <div class="crown">
+            <h2>Course Basket</h2>
+            
+            <!-- Roster Selector -->
+            <!-- courses will be loaded according to selected roster -->
+            <label>Roster:</label>
+            <select v-model="selectedRoster">
+                <option v-for="roster in rosterList">{{ roster }}</option>
+            </select>
+
+            <!-- Search Bar -->
+            <!-- TODO: search courses by code or title -->
+        </div>
+    </div>
+
+    <!-- 4 columns, one column per year  -->
+    <div class="course-columns">
+        <div class="column">
+            <!-- TODO -->
+        </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      rosterList: [],
+      selectedRoster: "",
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
