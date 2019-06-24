f77073fdb4767ac1c4e564d3b0bd6b119109c6f2
Pinxuan Huang <pinxhuang@gmail.com>
2019-06-24 14:31:26 +0400

    using vuefire

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { firestore } from 'firebase'
+import { firestorePlugin } from 'vuefire'
+
+// turns off the 'You are running Vue in development mode.' msg
+Vue.config.productionTip = false;
+
+Vue.use(firestore, firestorePlugin);
 
 new Vue({
   el: '#app',
