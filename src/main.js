294dc4ca826ff4b2128963f2b4c43e2fa7ad8d49
Pinxuan Huang <pinxhuang@gmail.com>
2019-07-06 19:48:59 -0700

    added vue-resource for http requests

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource'
+import VueFirestore from 'vue-firestore'
 import { firestore } from 'firebase'
 import { firestorePlugin } from 'vuefire'
 
 // turns off the 'You are running Vue in development mode.' msg
 Vue.config.productionTip = false;
 
-Vue.use(firestore, firestorePlugin);
+// VueResource - handles HTTP requests
+// VueFirestore - able to export firestore "db" in "firebase.js"
+Vue.use(VueResource, VueFirestore, firestore, firestorePlugin);
 
 new Vue({
   el: '#app',
