110a1156c464c89d30a76136c6e2cf0f531f4dac
Pinxuan Huang <pinxhuang@gmail.com>
2019-07-17 21:57:47 -0700

    added CourseModal

---
+++ src/components/CourseModal.vue
@@ -0,0 +1,66 @@
+<template>
+  <transition name="modal-fade">
+    <div class="modal-backdrop">
+      <div class="modal"
+        role="dialog"
+        aria-labelledby="modalTitle"
+        aria-describedby="modalDescription"
+      >
+        <header
+          class="modal-header"
+          id="modalTitle"
+        >
+          <slot name="header">
+            This is the default tile!
+          </slot>
+
+            <button
+              type="button"
+              class="btn-close"
+              @click="close"
+              aria-label="Close modal"
+            >
+              x
+            </button>
+        </header>
+        <section
+          class="modal-body"
+          id="modalDescription"
+        >
+          <slot name="body">
+            I'm the default body!
+          </slot>
+        </section>
+        <footer class="modal-footer">
+          <slot name="footer">
+            I'm the default footer!
+
+            <button
+              type="button"
+              class="btn-green"
+              @click="close"
+              aria-label="Close modal"
+            >
+              Close me!
+            </button>
+          </slot>
+        </footer>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+  export default {
+    name: 'modal',
+    methods: {
+      close() {
+        this.$emit('close');
+      },
+    },
+  };
+</script>
+
+<style>
+
+</style>
