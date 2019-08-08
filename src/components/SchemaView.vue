c95e8dface0949fbc7140e04a8c7a2f7ff0c4b55
Pinxuan Huang <pinxhuang@gmail.com>
2019-08-09 00:03:13 +0400

    added SchemaView

---
+++ src/components/SchemaView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+      <div>
+        <h2>Create a New Schema</h2>
+        <div>
+            <h3>Schema Name:</h3>
+            <!-- string input -->
+        </div>
+      </div>
+
+      <div>
+        <h2>Load Schema</h2>
+        <div>
+            <h3>Schema Key:</h3>
+            <!-- string input -->
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+        schemaName: '',
+        schemaKey: '',
+        generatedSchemaKey: '',
+    }
+  },
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
