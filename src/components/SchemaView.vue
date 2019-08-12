3f6d2cf8d04302583a2fc87adfe1dff601ad3d62
Pinxuan Huang <pinxhuang@gmail.com>
2019-08-12 18:00:38 +0400

    done SchemaView inputs

--- src/components/SchemaView.vue
+++ src/components/SchemaView.vue
@@ -1,35 +1,85 @@
 <template>
   <div>
-      <div>
-        <h2>Create a New Schema</h2>
-        <div>
-            <h3>Schema Name:</h3>
-            <!-- string input -->
-        </div>
+    <!-- Create a New Schema -->
+    <form>
+      <div v-show="step === 1">
+        <h1>Step One</h1>
+        <p>
+          <h2>{{ schemaName }}</h2>
+          <legend for="name">Schema Name:</legend>
+          <input id="name" name="name" v-model="schemaName" />
+        </p>
+
+        <button @click.prevent="next()">Next</button>
       </div>
 
-      <div>
-        <h2>Load Schema</h2>
-        <div>
-            <h3>Schema Key:</h3>
-            <!-- string input -->
-        </div>
+      <div v-show="step === 2">
+        <h1>Step Two</h1>
+        <p>
+          <legend for="street">Schema Key: {{ schemaKey }}</legend>
+          <!-- Display schemaKey -->
+          <!-- Let users know: Save this key to use it to access 
+          your schema in the future!-->
+          <p>Save this key to use it to access your schema in the future!</p>
+        </p>
+
+        <!-- Do not allow users to go back 
+        to avoid running "createSchema()" multiple times -->
+        <!-- <button @click.prevent="prev()">Previous</button> -->
+        <input type="submit" value="Done" />
       </div>
+    </form>
   </div>
 </template>
 
 <script>
+import db from "../firebase.js";
+
 export default {
-  data () {
+  data() {
     return {
-        schemaName: '',
-        schemaKey: '',
-        generatedSchemaKey: '',
-    }
+      step: 1,
+      schemaName: "",
+      schemaKey: "",
+      generatedSchemaKey: ""
+    };
   },
-}
+
+  methods: {
+
+    // Creates a new schema with "schemaName" as name
+    createSchema() {
+      var self = this;
+
+      db.collection("schemas").add({
+        name: this.schemaName,
+      })
+      .then(function(docRef) {
+          console.log("Schema created with ID: ", docRef.id);
+          // NOTE: "this.schemaKey" gives undefined 
+          // so we must use "self.schemaKey"
+          self.schemaKey = docRef.id
+          console.log("schemaKey: " + self.schemaKey)
+      })
+      .catch(function(error) {
+          console.error("Error adding schema: ", error);
+      });
+    },
+
+    // Goes to next step of multistep form
+    next() {
+        this.createSchema()
+        this.step++
+    },
+
+    // Takes user to GroupDnDFirestore
+    submit() {
+      // Pass schemaKey to GroupDnDFirestore
+      console.log("submitted!");
+    }
+  }
+};
 </script>
 
 <style scoped lang="scss">
-
 </style>
